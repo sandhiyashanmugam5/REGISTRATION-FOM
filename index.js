613f9d2d01a2c315ad7b797c5a066d1be32d1109
sandhiya <sandhiyashanmugan5@gmail.com>
2024-07-09 15:51:17 +0530

    COMMIT

---
+++ index.js
@@ -0,0 +1,14 @@
+function getInputsValues(e){
+    e.preventDefault()
+    let firstname = document.getElementById("firstname").value
+    let lastname = document.getElementById("lastname").value
+    let datofbirth = document.getElementById("dateofbirth").value
+    let email = document.getElementById("email").value
+    let password = document.getElementById("password").value
+    console.log(firstname, lastname, email, datofbirth, password);
+}
+
+
+function test(a,b,c){
+console.log();
+}
